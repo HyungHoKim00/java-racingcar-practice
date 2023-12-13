6be0fc3647655ecfba6a8a92d332c52164a14b74
HyungHoKim <hkim1109@naver.com>
2023-12-13 09:51:58 +0900

    feat(InputView): 전진 시도 횟수 입력
    전진 시도 횟수를 입력받는 readTries 생성

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,8 +6,15 @@ public class InputView {
     private static final String POSITIVE_INTEGER_PATTERN = "^[1-9]\\d*$";
     private static final String ERR_MSG_NOT_POSITIVE_INTEGER = "입력이 양의 정수가 아닙니다.";
 
-    public String carName() {
+    public String readCarNames() {
         return Console.readLine();
     }
 
+    public int readTries() {
+        String input = Console.readLine();
+        if (!input.matches(POSITIVE_INTEGER_PATTERN)) {
+            throw new IllegalArgumentException(ERR_MSG_NOT_POSITIVE_INTEGER);
+        }
+        return Integer.parseInt(input);
+    }
 }
